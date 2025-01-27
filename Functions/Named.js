637ec783fc906abd92eef8999742e6bc4a2f7d96
slx <sufailxd@gmail.com>
2025-01-28 02:55:03 +0530

    first commit

---
+++ Functions/Named.js
@@ -0,0 +1,74 @@
+// function greet(name){
+//     console.log("Hello " + name );
+// }
+// greet("slx")
+
+// -------------------------------------------------------------
+
+// function sum(a, b){
+//     return a + b 
+// }
+// console.log(sum(10 , 90))
+
+// function sum(a, b){
+//     console.log("Sum is: " + (a , b))
+// }
+// sum(10, 90)
+
+// -------------------------------------------------------------
+// Questions:-
+// ------------
+// 1) Question: Write a function called greetUser that takes a name as an argument and prints "Hello, [name]!" to the console.
+
+    //    function greetUser(name){
+    //     console.log("Hello " + name + "!");
+    //    } 
+    //    greetUser("SLX")
+
+// 2) Write a function called isEven that takes a number as an argument and returns true if the number is even, and false if it is odd.    
+   
+// function isEven(no){
+    //     return no % 2 == 0
+    // }
+    // console.log(isEven(2));
+    // console.log(isEven(3));
+
+// 3) Write a function called fibonacci that takes a number n as an argument and returns the nth number in the Fibonacci sequence. (The Fibonacci sequence starts with 0, 1, 1, 2, 3, 5, 8, 13,...)    
+    
+// function fibonacci(n) {
+//     if (n <= 1) {
+//         return n; 
+//     }
+//     return fibonacci(n - 1) + fibonacci(n - 2); 
+// }
+
+// console.log(fibonacci(5));
+// console.log(fibonacci(6));
+
+// 4) Write a function called reverseString that takes a string as an argument and returns the string in reverse order.
+
+//    function reverseString(str) {
+//     let rev = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         rev += str[i]; 
+//     }
+//     return rev
+//    }
+//    console.log(reverseString("SLX"))
+
+// 5) Write a function called removeDuplicates that takes an array as an argument and returns a new array with duplicate values removed.
+          
+    //  function removeDuplicates(arr){
+    //     let unique = []
+    //     for(let i=0; i<arr.length; i++){
+    //         if(!unique.includes(arr[i])){
+    //             unique.push(arr[i])
+    //         }
+    //     }
+    //     return unique
+    //  }
+    //  console.log(removeDuplicates([1,2,2,3,4,4,5]));
+     
+// -------------------------------------------------------------
+// -------------------------------------------------------------
+// -------------------------------------------------------------
