a583244cede398147ea447142b6e5eaf96fbed27
slx <sufailxd@gmail.com>
2025-01-29 10:24:39 +0530

    js

---
+++ Functions/Arrow.js
@@ -0,0 +1,40 @@
+    // const functionName  = (param) => {
+    //     return param
+    // }
+    // console.log(add(param));
+
+    
+// -------------------------------------------------------------
+// Questions:-
+// ------------
+
+// 1) Write an arrow function that takes two numbers as parameters and returns their sum.
+   
+    // const sum = (a, b) => a + b
+    // console.log(sum(10,90));
+    // ----------------------------
+
+    // const sum = (a, b) => {
+    //     return a + b
+    // }
+    // console.log(sum(10, 90));
+
+// 2) Write an arrow function that takes an array of numbers and returns a new array with all the numbers doubled.
+    
+    // const double = (arr) => arr.map(num => num * 2)
+    // const nos = [1,2,3,4,5]
+    // console.log(double(nos));
+    
+// 3) Write an arrow function that filters out all odd numbers from an array and returns the even numbers in a new array. Make sure to use arrow functions for both the filtering and the callback within filter(). 
+    
+    // const odd = (arr) => arr.filter(num => num % 2 == 0)
+    // const nos = [1,2,3,4,5,6]
+    // console.log(odd(nos));
+    
+// 4) Write an arrow function that takes an array of numbers and returns the sum of all the numbers. Use the reduce() method.
+
+    // const sum = (arr) => arr.reduce((acc, current) => acc + current, 0)
+    // const nos = [1,2,3,4,5]
+    // console.log(sum(nos));
+    
+// -------------------------------------------------------------
